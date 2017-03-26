b2595b93ef17f6f76c918a9a61551411016a58fb
Cristobal Moya <torilix@Cristobals-MacBook-Pro.local>
2017-03-26 18:30:02 -0300

    imperonal

---
+++ impersonal.py
@@ -0,0 +1,29 @@
+import Vida
+import Perseverancia
+import Curiosidad
+import Maestria
+import Estrellas
+
+
+#Ingredientes basicos para alcanzar las estrellas:
+
+
+Vida.aprendizaje = Curiosidad * Perseverancia
+
+#Hambre de conocimiento
+
+
+while not Vida.exito():
+    Vida.intentar()
+
+#Seguir intentando
+
+if Maestria.alcanzada == True:
+    Vida.descansar()
+
+#Alcanzar la maestria
+
+Estrellas.alcanzadas = True
+
+
+
